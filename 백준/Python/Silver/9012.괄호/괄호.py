227e141f76d769fe7356a40de9034bb465b0f284
김유석(Yu Seok Kim) <kys9808@pusan.ac.kr>
2026-04-22 02:39:17 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python/Silver/9012.괄호/괄호.py
+++ 백준/Python/Silver/9012.괄호/괄호.py
@@ -1,21 +1,16 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
-
 n=int(input())
 for x in range(0,n):
-    data=input().rstrip()
-    p=deque()
+    data=input()
+    p=0
     test=True
-    for k in data:
-        if k=='(':
-            p.append(k)
+    for k in range(0,len(data)):
+        if data[k]=='(':
+            p=p+1
         else:
-            try:
-                p.pop()
-            except:
-                test=False
-    if len(p)>0:
+            p=p-1
+        if p<0:
+            test=False
+    if p!=0:
         test=False
     if test:
         print('YES')
