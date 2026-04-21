039eada24086a877c26317e5c523ab6b95c8b29d
김유석(Yu Seok Kim) <kys9808@pusan.ac.kr>
2026-04-22 02:57:15 +0900

    [Silver IV] Title: solved.ac, Time: 208 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Python/Silver/18110.solved．ac/solved．ac.py
+++ 백준/Python/Silver/18110.solved．ac/solved．ac.py
@@ -2,18 +2,15 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 if n==0:
     print(0)
-elif n<4:
-    summation=0
-    for i in range(0,n):
-        summation=summation+int(sys.stdin.readline().rstrip())
-    print(int(0.5+summation/n))
 else:
     rates=[]
     s=0
     for i in range(0,n):
         rates.append(int(sys.stdin.readline().rstrip()))
-    s=int(0.5+n*0.15)
-    rates.sort()
-    rates=rates[s:-s]
-    print(int(0.5+(sum(rates)/(n-2*s))))
-#list negative index로 오른끝 잡으면 그냥 되는 거였네;;;
+    if n<4:
+        print(int((sum(rates)/n)+0.5))
+    else:
+        s=int(0.5+n*0.15)
+        rates.sort()
+        rates=rates[s:-s]
+        print(int(0.5+(sum(rates)/(n-2*s))))
