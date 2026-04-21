f18908ff196f955d31d5a3a081ee636f492ac11b
김유석(Yu Seok Kim) <kys9808@pusan.ac.kr>
2026-04-22 02:53:42 +0900

    [Silver V] Title: 국회의원 선거, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Python/Silver/1417.국회의원 선거/국회의원 선거.py
+++ 백준/Python/Silver/1417.국회의원 선거/국회의원 선거.py
@@ -1,25 +1,23 @@
 import sys
 input=sys.stdin.readline
-import heapq
-
 n=int(input())
 if n==1:
     input()
     print(0)
 else:
-    me = int(input())
-    votes = []
-    for _ in range(n - 1):
-        num = int(input())
-        heapq.heappush(votes, (-num, num))
-#최소 힙 정렬을 -num 기준으로 돌리고 입력을 num을 하면 최대 힙이 되어요
-    count = 0
-    while True:
-        leader = heapq.heappop(votes)[1]
-        if leader >= me:
-            count += 1
-            me += 1
-            heapq.heappush(votes, (-leader +1, leader -1))
-        else:
-            break
-    print(count)
+    votes=[0]*n
+    me=int(input())
+    for x in range(1,n):
+        votes[x]=int(input())
+    rival=max(votes)
+    if me-rival>0:
+        print(0)
+    else:
+        count=0
+        while rival-me>=0:
+            idx=votes.index(rival)
+            me=me+1
+            count=count+1
+            votes[idx]=rival-1
+            rival=max(votes)
+        print(count)
