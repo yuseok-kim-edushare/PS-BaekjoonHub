fa951b9a6cfed715d7c5d430f6ecbcc704f85f0c
김유석(Yu Seok Kim) <kys9808@pusan.ac.kr>
2026-04-22 01:56:56 +0900

    [Silver III] Title: 터렛, Time: 52 ms, Memory: 6196 KB -BaekjoonHub

--- 백준/C#/Silver/1002.터렛/터렛.cs
+++ 백준/C#/Silver/1002.터렛/터렛.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace BOJ_C_sharp
 {
     internal class Program_1002
@@ -5,38 +7,38 @@ namespace BOJ_C_sharp
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int x1, y1, r1, x2, y2, r2, L2, rR, rr;
-
+            StringBuilder sb = new StringBuilder();
+            
             for (int i = 0; i < n; i++)
-            {
-                string s = Console.ReadLine();
-                string[] ss = s.Split();
-                x1 = int.Parse(ss[0]);
-                y1 = int.Parse(ss[1]);
-                r1 = int.Parse(ss[2]);
-                x2 = int.Parse(ss[3]);
-                y2 = int.Parse(ss[4]);
-                r2 = int.Parse(ss[5]);
-                L2 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-                rR = (r1 + r2) * (r1 + r2);
-                rr = (r1 - r2) * (r1 - r2);
-                if (L2 == 0 && r1 == r2)
                 {
-                    Console.WriteLine(-1);
+                    string[] ss = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    int x1 = int.Parse(ss[0]);
+                    int y1 = int.Parse(ss[1]);
+                    int r1 = int.Parse(ss[2]);
+                    int x2 = int.Parse(ss[3]);
+                    int y2 = int.Parse(ss[4]);
+                    int r2 = int.Parse(ss[5]);
+
+                    int dx = x1 - x2;
+                    int dy = y1 - y2;
+                    int L2 = dx * dx + dy * dy;
+                    int rR = (r1 + r2) * (r1 + r2);
+                    int rr = (r1 - r2) * (r1 - r2);
+
+                    int result;
+                    if (L2 == 0 && r1 == r2)
+                        result = -1;
+                    else if (rR > L2 && L2 > rr)
+                        result = 2;
+                    else if (rR == L2 || rr == L2)
+                        result = 1;
+                    else
+                        result = 0;
+
+                    sb.AppendLine(result.ToString());
                 }
-                else if (rR > L2 && L2 > rr)
-                {
-                    Console.WriteLine(2);
-                }
-                else if (rR == L2 || rr == L2)
-                {
-                    Console.WriteLine(1);
-                }
-                else
-                {
-                    Console.WriteLine(0);
-                }
-            }
+
+             Console.Write(sb.ToString());
         }
     }
 }
