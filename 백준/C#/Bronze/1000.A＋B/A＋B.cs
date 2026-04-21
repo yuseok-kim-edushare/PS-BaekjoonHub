9614aaba113b4805aa5e7d69e0744d78f5ec45b4
김유석(Yu Seok Kim) <kys9808@pusan.ac.kr>
2026-04-22 01:56:39 +0900

    [Bronze V] Title: A+B, Time: 60 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/C#/Bronze/1000.A＋B/A＋B.cs
@@ -0,0 +1,10 @@
+using System;
+class PS{
+    static void Main() {
+        string s = Console.ReadLine();
+        string[] ss = s.Split();
+        int a = int.Parse(ss[0]);
+        int b = int.Parse(ss[1]);
+        Console.WriteLine(a+b);
+    }
+}
