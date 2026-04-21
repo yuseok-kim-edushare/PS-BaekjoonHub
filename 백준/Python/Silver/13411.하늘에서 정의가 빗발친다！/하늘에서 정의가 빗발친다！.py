5f7e10b9e782a36ca316dd9831f60ffdcb76b6a7
김유석(Yu Seok Kim) <kys9808@pusan.ac.kr>
2026-04-22 02:53:50 +0900

    [Silver IV] Title: 하늘에서 정의가 빗발친다!, Time: 340 ms, Memory: 44460 KB -BaekjoonHub

--- 백준/Python/Silver/13411.하늘에서 정의가 빗발친다！/하늘에서 정의가 빗발친다！.py
+++ 백준/Python/Silver/13411.하늘에서 정의가 빗발친다！/하늘에서 정의가 빗발친다！.py
@@ -4,8 +4,8 @@ n=int(input())
 kills=[[0,0]]*n
 for i in range(0,n):
     x,y,v=map(int,input().split())
-    r2=(x**2+y**2)
-    kills[i]=[r2/v/v,i+1]
+    r=(x**2+y**2)**(0.5)
+    kills[i]=[r/v,i+1]
 kills.sort()
 for y in range(0,n):
     print(kills[y][1])
