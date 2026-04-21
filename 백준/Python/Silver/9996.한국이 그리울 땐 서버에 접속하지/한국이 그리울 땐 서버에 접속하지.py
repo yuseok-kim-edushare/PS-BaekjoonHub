8b9f7cb5a77d9ad5342b3a22725af23b0b230548
김유석(Yu Seok Kim) <kys9808@pusan.ac.kr>
2026-04-22 02:57:10 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 120 ms, Memory: 34736 KB -BaekjoonHub

--- 백준/Python/Silver/9996.한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
+++ 백준/Python/Silver/9996.한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
@@ -1,9 +1,8 @@
 import re
 n = int(input())
-patterns=input().rstrip().split('*')
-le=len(patterns)
-p =patterns[0]+'[a-z]*'+patterns[1]
-pp = re.compile(p)
+ps=input().split('*')
+p=ps[0]+'[a-z]*'+ps[1]
+pp=re.compile(p)
 for x in range(0,n):
     name=input()
     if None==re.fullmatch(pp,name):
